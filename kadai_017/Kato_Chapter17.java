26087b70aeb9cf1be3bfec22c711ed9e73881e12
FujiiYuki3 <151428461+FujiiYuki3@users.noreply.github.com>
2024-01-18 15:40:18 +0900

    「Kato_Chapter17.java」を再プッシュ

--- kadai_017/Kato_Chapter17.java
+++ kadai_017/Kato_Chapter17.java
@@ -1,21 +1,19 @@
 package kadai_017;
 
 public abstract class Kato_Chapter17 {
-	public String familyName; //姓を表す
-	public String givenName; //名を表す
-	public String address; //住所を表す
+	public String familyName = "加藤"; //姓を表す
+	public String givenName = ""; //名を表す
+	public String address = "東京都中野区○×"; //住所を表す
 	
 	public void commonIntroduce() {
-		familyName = "加藤";
-		address = "東京都中野区○×";
+		System.out.println("名前は" + this.familyName + this.givenName + "です");
+		System.out.println("住所は" + this.address + "です");
 	}
 	
 	public abstract void eachIntroduce();
 
 	public void execIntroduce() {
 		commonIntroduce();
-		System.out.println("名前は" + this.familyName + this.givenName + "です");
-		System.out.println("住所は" + this.address + "です");
 		eachIntroduce();
 	}
 
